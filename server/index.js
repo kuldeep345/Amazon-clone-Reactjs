6507878d792f266eaca4266afbf58c532fc893a3
kuldeepSingh <singhkuldeep7850@gmail.com>
2023-01-29 11:02:27 +0530

    project done

---
+++ server/index.js
@@ -0,0 +1,20 @@
+require("dotenv").config()
+const express = require('express')
+const app = express()
+const cors = require('cors')
+require('./db/conn')
+const cookieParser = require('cookie-parser')
+
+app.use(express.json())
+app.use(cookieParser(""))
+app.use(cors())
+
+
+app.use('/products' , require('./routes/product'))
+app.use('/user' , require('./routes/user'))
+
+const port = process.env.PORT || 5000;
+
+app.listen(port , ()=>{
+    console.log(`server is running on http://localhost:${port}`)
+})
